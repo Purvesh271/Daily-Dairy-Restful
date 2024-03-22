165de6753ed3fea0f7bf717beae5926f7a78590f
Purvesh271 <ppurvesh923@gmail.com>
2024-03-23 01:49:52 +0530

    Edit entry feature using patch and method override

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express =  require("express");
 const app = express();
 const port = 8080;
+const { v4: uuidv4 } = require('uuid');
+const methodOverride = require("method-override");
 
 app.set("view engine","ejs");
 app.use(express.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
 
 const path = require("path");
 app.set("views", path.join(__dirname,"/views"));
@@ -16,24 +19,24 @@ app.listen(port,()=>{
 
 let posts = [
     {
-        id:"1",
+        id:uuidv4(),
         date:"01 April 2024",
         content:"Today was a great day, I got my driving liecence"
     },
     {
-        id:"2",
+        id:uuidv4(),
         date:"02 April 2024",
         content:"Today I got my first internship"
     },
     {
-        id:"3",
+        id:uuidv4(),
         date:"03 April 2024",
         content:"Today I learned about RestApis"
     },
 
 ];
 
-//index route
+//index route 
 app.get("/posts",(req,res)=>{
     res.render("index.ejs",{posts});
 });
@@ -45,7 +48,8 @@ app.get("/posts/new",(req,res)=>{
 
 app.post("/posts",(req,res)=>{
     let {date,content} = req.body;
-    posts.push({date,content});
+    let id = uuidv4();
+    posts.push({id,date,content});
     res.redirect("/posts");
 });
 
@@ -54,4 +58,19 @@ app.get("/posts/:id",(req,res)=>{
     let {id}= req.params;
     let post = posts.find((p)=> id === p.id);
     res.render("show.ejs",{post});
+});
+
+//Edit route (edit an entry)
+app.patch("/posts/:id",(req,res)=>{
+    let {id} = req.params;
+    let newContent = req.body.content;
+    let post = posts.find((p)=> id === p.id);
+    post.content = newContent;
+    res.redirect("/posts");
+});
+
+app.get("/posts/:id/edit", (req,res)=>{
+    let {id} = req.params;
+    let post = posts.find((p)=> id === p.id);
+    res.render("edit.ejs",{post});
 });
